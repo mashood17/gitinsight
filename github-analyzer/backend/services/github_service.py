e4760dba4cba8c2ebcc52e00936e978c87fe9fc7
Mahammad Mashood <mshdrnj17@gmail.com>
2026-03-12 06:25:41 +0530

    Add GitHub token authentication

--- github-analyzer/backend/services/github_service.py
+++ github-analyzer/backend/services/github_service.py
@@ -1,22 +1,19 @@
 import requests
+import os
 
 GITHUB_API = "https://api.github.com/users"
 
 HEADERS = {
-    "User-Agent": "GitInsight-App"
+    "User-Agent": "GitInsight-App",
+    "Authorization": f"token {os.getenv('GITHUB_TOKEN')}"
 }
 
 def get_user_profile(username):
 
     url = f"{GITHUB_API}/{username}"
-
     response = requests.get(url, headers=HEADERS)
 
-    if response.status_code == 404:
-        return None
-
     if response.status_code != 200:
-        print("GitHub API error:", response.status_code, response.text)
         return None
 
     return response.json()
@@ -25,11 +22,9 @@ def get_user_profile(username):
 def get_user_repositories(username):
 
     url = f"{GITHUB_API}/{username}/repos"
-
     response = requests.get(url, headers=HEADERS)
 
     if response.status_code != 200:
-        print("GitHub repos error:", response.status_code, response.text)
         return []
 
     return response.json()
